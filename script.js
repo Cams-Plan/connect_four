d2cbf40745b42e1fd6747640fb0554a6e25b0f52
Cams-Plan <ca.francis@outlook.com>
2023-07-28 11:10:52 +0100

    question comment added to script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const timer = document.querySelector('#display-timer');
 const turnButton = document.querySelector('#turn-button')
 
-
+// how will we trigger the start of the game on the html? from the timer? or new button?
 
 
 const changePlayer = (player) => {
